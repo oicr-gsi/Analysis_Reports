2141839e37482d4d1081017652f101e0c0f9e20d
Julia Qian <jqian@oicr.on.ca>
2022-09-16 14:09:29 -0400

    GBS-3624: added parsing to extract data from bcftools

--- summarize_vcf.py
+++ summarize_vcf.py
@@ -3,23 +3,28 @@ import re
 import json
 import argparse
 
-
-def get_file_names(input_file):
+def get_files(input_json):
     '''
     (str) -> list[str]
 
-    Returns the file names specified in input_file as a list
+    Extracts the file paths from input_json
 
     Parameters
     ----------
-    - input_file (str): path to the file that contains the vcf file names 
+    - input_json (str): path to the file that contains the vcf files
 
     '''
-    with open(input_file) as file:
-        file_names = file.readlines()
-        file_names = [line.rstrip() for line in file_names]
+    data = {}
+    files = []
+
+    with open(input_json) as file:
+        data = json.load(file)
     file.close()
-    return file_names
+
+    #get mutect2 files
+    files = [ case["mutations"]["file"] for case in data["cases"].values() if "mutations" in case.keys()]
+
+    return files
 
 
 def get_num_calls(file):
@@ -81,31 +86,63 @@ def get_key(regex, full_name):
     return match.group() if match is not None else full_name
 
 
+def get_sn(file):
+    '''
+    (str) -> dict
 
-def get_json_data(input_file, file_re):
+    Selects statistics from the SN section of the bcftools summary generated from file
+
+    Parameters
+    ----------
+    - file (str): path to the vcf file to be analyzed
+
+    '''
+    sn_stats = {}
+    target_stats = ["records", "SNPs", "MNPs", "indels"]
+
+    regex = "^SN.*" + target_stats[0]
+    for stat in target_stats[1:]:
+        regex = regex + "|" + "^SN.*"+ stat
+
+    data = sp.check_output(
+        "bcftools stats -f \"PASS\" " + file + "|grep -E '" + regex + "' |awk '{print $(NF)}'",
+        shell=True
+        ).decode('ascii').strip()
+    data = data.split("\n")
+    
+    for count, stat in enumerate(target_stats):
+        sn_stats[stat] = int(data[count])
+
+    return sn_stats
+
+
+def get_json_data(input_json, file_re):
     '''
     (str, str) -> str
 
     Returns a json object with keys as the file names abbreviated to the regex
-    specified by file_re, containing the number of calls and the number of PASS calls.
+    specified by file_re, containing summary data.
 
     Parameters
     ----------
-    - input_file (str): file containing the paths to the vcf files to be analyzed
+    - input_json (str): file containing the paths to the vcf files to be analyzed
     - file_re (str): regex specifying the abbreviated name
 
     '''
-
-    vcf_file_names = get_file_names(input_file)
-
     data_dict = {}
-    for file in vcf_file_names:
-        if get_key(file_re, file) in data_dict:
+    
+    vcf_files = get_files(input_json)
+
+    for counter, file in enumerate(vcf_files):
+        key = get_key(file_re, file)
+        if key in data_dict:
             raise Exception("Duplicate file name, exiting...")
         else:
-            data_dict[get_key(file_re, file)] = { "data" : {} }
-            data_dict[get_key(file_re, file)]["data"]["num_calls"] = get_num_calls(file)
-            data_dict[get_key(file_re, file)]["data"]["num_pass"] = get_num_pass(file)
+            data_dict[key] = { "data" : {} }
+            data_dict[key]["data"]["num_calls"] = get_num_calls(file)
+            data_dict[key]["data"]["num_pass"] = get_num_pass(file)
+            data_dict[key]["data"]["bcf_PASS_summary"] = get_sn(file)
+
 
     with open('output.json', 'w', encoding='utf-8') as file:
         json.dump(data_dict, file, ensure_ascii=False, indent=4)
