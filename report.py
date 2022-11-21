f20bea026ba972b158c820c24cef8c7d0e9d1f1c
Julia Qian <jqian@oicr.on.ca>
2022-11-21 17:03:34 -0500

    refactored to use classes

--- report.py
+++ report.py
@@ -15,26 +15,186 @@ import sys
 import pathlib
 from weasyprint import HTML
 from weasyprint import CSS
+from typing import Dict, Type, List, Callable, Union, Tuple, Set, Any
 
 db = f"/scratch2/groups/gsi/staging/qcetl_v1/"
 
 
-class Proccess:
-    columns: {}
-    headers: {}
-    get_data: {}
-    
+class Process:
+    data = {}
+    sample_ids = {}
+    context = {}
+    processes = {
+        "mutect2": "mutect2_matched_by_tumor_group",
+        "delly": "delly_matched_by_tumor_group",
+        "sequenza": "sequenza_by_tumor_group",
+        "rsem": "rsem",
+        "starfusion": "starfusion",
+        "metadata": "mutect2_matched_by_tumor_group"
+    }
+
+    columns: Dict[str, int]
+    headers: Dict[str, str]
+    cache_name: str
+    table_name: str
+    name: str
+    blurb: str
+    section: str
+
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        con = sqlite3.connect(db + f"{self.cache_name}/latest")
+        cur = con.cursor()
+
+        data =  {id: {} for id in self.sample_ids}
+
+        for id in self.sample_ids:
+            wfr = self.data[id][self.processes[self.section]]["wfrun"]
+
+            for row in cur.execute( # forloop automatically ignores empty SELECTs
+                f"""
+                select * from {self.table_name} where "Workflow Run SWID" like '%{wfr}%';
+                """):
+                data[id]["sample_id"] = id
+                for column in self.columns.keys():
+                    data[id][column] = (
+                        row[self.columns[column]]
+                        if isinstance(row[self.columns[column]], str)
+                        else round(row[self.columns[column]], 2)
+                    )
+        
+        cur.close()
+        con.close()
+        return data
+
+    def load_context(self):
+        Process.context[self.section + "_data"] = self.get_data()
+        Process.context[self.section + "_headers"] = self.headers
+        Process.context[self.section + "_blurb"] = self.blurb
+
+
+class StarFusion(Process):
+    def __init__(self):
+        Process.__init__(self)
+        self.cache_name = "analysis_starfusion"
+        self.table_name = "analysis_starfusion_analysis_starfusion_1"
+        self.section= "starfusion"
+        self.columns = {
+            "num_records": 8,
+        }
+
+        self.headers = {
+            "sample_id": "Sample ID",
+            "num_records": "Number of Records",
+        }
+        self.blurb = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur sit amet commodo lorem. Vivamus mollis mauris urna, eget volutpat ex pharetra vel. Suspendisse ultrices commodo feugiat. Morbi aliquam semper ligula eget interdum. Cras vestibulum tempus rutrum.
+            """
+
+class RSEM(Process):
+    def __init__(self):
+        Process.__init__(self)
+        self.cache_name = "analysis_rsem"
+        self.table_name = "analysis_rsem_analysis_rsem_1"
+        self.section = "rsem"
+        self.columns = {
+            "total": 8,
+            "pct_non_zero": 9,
+            "Q0": 10,
+            "Q0.05": 11,
+            "Q0.1": 12,
+            "Q0.25": 13,
+            "Q0.5": 14,
+            "Q0.75": 15,
+            "Q0.9": 16,
+            "Q0.95": 17,
+            "Q1": 18,
+        }
+
+        self.headers = {
+            "sample_id": "Sample ID",
+            "total": "Total",
+            "pct_non_zero": "Percent of Non-Zero Records",
+            "Q0": "Q0",
+            "Q0.05": "Q0.05",
+            "Q0.1": "Q0.1",
+            "Q0.25": "Q0.25",
+            "Q0.5": "Q0.5",
+            "Q0.75": "Q0.75",
+            "Q0.9": "Q0.9",
+            "Q0.95": "Q0.95",
+            "Q1": "Q1",
+        }
+
+        self.blurb = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur sit amet commodo lorem. Vivamus mollis mauris urna, eget volutpat ex pharetra vel. Suspendisse ultrices commodo feugiat. Morbi aliquam semper ligula eget interdum. Cras vestibulum tempus rutrum.
+            """
+
+class Mutect2(Process):
+    def __init__(self):
+        Process.__init__(self)
+        self.cache_name = "analysis_mutect2"
+        self.table_name = "analysis_mutect2_analysis_mutect2_1"
+        self.section = "mutect2"
+        self.columns = {
+            "num_calls": 8,
+            "num_PASS": 9,
+            "num_SNPs": 10,
+            "num_multi_SNPs": 11,
+            "num_indels": 12,
+            "titv_ratio": 13,
+            "pct_ti": 15,
+            "pct_tv": 16,
+            "num_MNPs": 14,
+        }
+
+        self.headers = {
+            "sample_id": "Sample ID",
+            "num_calls": "Number of Calls",
+            "num_PASS": "Number of PASS calls",
+            "num_SNPs": "Number of SNPs",
+            "num_multi_SNPs": "Number of multi-SNPs",
+            "num_indels": "Number of indels",
+            "titv_ratio": "TI/TV ratio",
+            "pct_ti": "Percent TI",
+            "pct_tv": "Percent of TV",
+            "num_MNPs": "Number of MNPs",
+
+        }
+        self.blurb = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur sit amet commodo lorem. Vivamus mollis mauris urna, eget volutpat ex pharetra vel. Suspendisse ultrices commodo feugiat. Morbi aliquam semper ligula eget interdum. Cras vestibulum tempus rutrum.
+            """
+
+class Metadata(Process):
+    def __init__(self):
+        Process.__init__(self)
+        self.cache_name = "analysis_mutect2"
+        self.table_name = "analysis_mutect2_analysis_mutect2_1"
+        self.section = "metadata"
+        self.columns = {
+            "group_id": 1,
+            "tissue_type": 5,
+            "tissue_origin": 6,
+            "library_design": 2,
+        }
+        self.headers = {
+            "sample_id": "Sample ID",
+            "group_id": "Group ID",
+            "tissue_type": "Tissue Type",
+            "tissue_origin": "Tissue Origin",
+            "library_design": "Library Design"
+        }
+        self.blurb = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Curabitur sit amet commodo lorem. Vivamus mollis mauris urna, eget volutpat ex pharetra vel.
+            Suspendisse ultrices commodo feugiat. Morbi aliquam semper ligula eget interdum.
+            Cras vestibulum tempus rutrum.
+            """
 
 
 
-
-processes = {
-    "mutect2_matched_by_tumour_group" : "mutect2",
-    "delly_matched_by_tumour_group" : "delly",
-    "sequenza_by_tumour_group": "sequenza",
-    "rsem": "rsem",
-    "starfusion": "starfusion"
-}
 
 def makepdf(html, outputfile):
     """
@@ -51,141 +211,32 @@ def makepdf(html, outputfile):
     htmldoc = HTML(string=html, base_url=__file__)
     htmldoc.write_pdf(outputfile, stylesheets=[CSS('./static/css/style.css')], presentational_hints=True)
 
-def get_starfusion_data(sample_ids, data, columns):
-    con = sqlite3.connect(db + "analysis_starfusion/latest")
-    cur = con.cursor()
-
-    starfusion_data =  {id: {} for id in sample_ids}
-
-    for id in sample_ids:
-        wfr = data[id]["starfusion"]["wfrun"]
-
-        for row in cur.execute( # forloop automatically ignores empty SELECTs
-            f"""
-            select * from analysis_starfusion_analysis_starfusion_1 where "Workflow Run SWID" like '%{wfr}%';
-            """):
-            starfusion_data[id]["sample_id"] = id
-            for column in columns.keys():
-                starfusion_data[id][column] = row[columns[column]]
-    
-    cur.close()
-    con.close()
-    return starfusion_data
-
-
 def generate_report():
-    data = {}
-    context = {}
     with open("IRIS.json") as f:
-        data = json.load(f)
+        Process.data = json.load(f)
     #     with open("input.json", "w") as fo:
     #         json.dump(data, fo, indent=4)
 
-    sample_ids = list(data.keys())
-    metadata = {id: {} for id in sample_ids}
+    Process.sample_ids = list(Process.data.keys())
 
-    context["metadata_headers"] = {
-        "sample_id": "Sample ID",
-        "group_id": "Group ID",
-        "tissue_type": "Tissue Type",
-        "tissue_origin": "Tissue Origin",
-        "library_design": "Library Design"
+    classes = {
+        "mutect2": Mutect2(),
+        "rsem": RSEM(),
+        "starfusion": StarFusion(),
+        "metadata": Metadata(),
     }
 
-    context["rsem_headers"] = {
-        "sample_id": "Sample ID",
-        "total": "Total",
-        "pct_non_zero": "Percent of Non-Zero Records",
-        "Q0": "Q0",
-        "Q0.05": "Q0.05",
-        "Q0.1": "Q0.1",
-        "Q0.25": "Q0.25",
-        "Q0.5": "Q0.5",
-        "Q0.75": "Q0.75",
-        "Q0.9": "Q0.9",
-        "Q0.95": "Q0.95",
-        "Q1": "Q1",
-    }
+    for p in classes.keys():
+        classes[p].load_context()
 
-    metadata_columns = {
-        "group_id": 1,
-        "tissue_type": 5,
-        "tissue_origin": 6,
-        "library_design": 2,
-    }
-
-    rsem_columns = {
-        "total": 8,
-        "pct_non_zero": 9,
-        "Q0": 10,
-        "Q0.05": 11,
-        "Q0.1": 12,
-        "Q0.25": 13,
-        "Q0.5": 14,
-        "Q0.75": 15,
-        "Q0.9": 16,
-        "Q0.95": 17,
-        "Q1": 18,
-    }
-
-    con = sqlite3.connect(db + "analysis_mutect2/latest")
-    cur = con.cursor()
-
-    for id in sample_ids:
-        wfr = data[id]["mutect2_matched_by_tumor_group"]["wfrun"]
-
-        for row in cur.execute( # forloop automatically ignores empty SELECTs
-            f"""
-            select * from analysis_mutect2_analysis_mutect2_1 where "Workflow Run SWID" like '%{wfr}%';
-            """):
-            metadata[id]["sample_id"] = id
-            for column in metadata_columns.keys():
-                metadata[id][column] = row[metadata_columns[column]]
-    context["metadata"] = metadata
-
-    cur.close()
-    con.close()
     
-    rsem_data = {id: {} for id in sample_ids}
-
-
-    con = sqlite3.connect(db + "analysis_rsem/latest")
-    cur = con.cursor()
-    for id in sample_ids:
-        wfr = data[id]["rsem"]["wfrun"]
-
-        for row in cur.execute( # forloop automatically ignores empty SELECTs
-            f"""
-            select * from analysis_rsem_analysis_rsem_1 where "Workflow Run SWID" like '%{wfr}%';
-            """):
-            rsem_data[id]["sample_id"] = id
-            for column in rsem_columns.keys():
-                rsem_data[id][column] = round(row[rsem_columns[column]], 2)
-
-    context["rsem_data"] = rsem_data
-
-
-    cur.close()
-    con.close()
-
-    starfusion_columns = {
-        "num_records": 8,
-    }
-
-    context["starfusion_data"] = get_starfusion_data(sample_ids, data, starfusion_columns)
-    context["starfusion_columns"] = starfusion_columns
-    context["starfusion_headers"] = {
-        "sample_id": "Sample ID",
-        "num_records": "Number of Records",
-    }
-
     with open('meta_context.json', 'w', encoding='utf-8') as file:
-        json.dump(context, file, ensure_ascii=False, indent=4)
+        json.dump(Process.context, file, ensure_ascii=False, indent=4)
 
     environment = Environment(loader=FileSystemLoader("templates/"))
     results_template = environment.get_template("metadata.html")
 
-    contents = results_template.render(context)
+    contents = results_template.render(Process.context)
 
     with open("meta_report.html", "w", encoding="utf-8") as results:
         results.write(contents)
